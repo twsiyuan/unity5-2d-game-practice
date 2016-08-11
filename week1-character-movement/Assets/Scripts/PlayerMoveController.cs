190c44cdd9c2c1ee136150d6c2d9c46f77982759
easy <easy0519@gmail.com>
2016-08-11 21:18:27 +0800

    week1 practice project

---
+++ week1-character-movement/Assets/Scripts/PlayerMoveController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class PlayerMoveController : MonoBehaviour {
+
+    public float MaxSpeed = 20;
+    public float MoveForce = 356;
+
+    public float JumpForce = 100;
+
+    bool jumpFlag = false;
+
+    void Update()
+    {
+        var gounded = this.GetComponent<GroundDetector>().Grounded;
+        if (Input.GetButtonDown("Jump") && gounded)
+        {
+            this.jumpFlag = true;
+        }
+    }
+
+	void FixedUpdate()
+    {
+        var dir = Input.GetAxis("Horizontal");
+        var r = this.GetComponent<Rigidbody2D>();
+        if (r.velocity.x * dir < this.MaxSpeed)
+        {
+            r.AddForce(Vector2.right * this.MoveForce * dir);
+        }
+
+        if (Mathf.Abs(r.velocity.x) > this.MaxSpeed)
+        {
+            r.velocity = new Vector2(Mathf.Sign(r.velocity.x) * this.MaxSpeed, r.velocity.y);
+        }
+
+        if (this.transform.localScale.x * dir < 0)
+        {
+            this.Flip();
+        }
+
+        if (this.jumpFlag)
+        {
+            r.AddForce(Vector2.up * this.JumpForce);
+            this.jumpFlag = false;
+        }
+   }
+
+    void Flip()
+    {
+        var s = this.transform.localScale;
+        s.x *= -1;
+        this.transform.localScale = s;
+    }
+}
