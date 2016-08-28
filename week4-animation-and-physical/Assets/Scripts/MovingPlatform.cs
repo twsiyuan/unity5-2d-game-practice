758eb0d6649ccbec17f7b308c61d90d5034154a4
easy <easy0519@gmail.com>
2016-08-28 22:25:45 +0800

    week4

---
+++ week4-animation-and-physical/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if (!this.enabled)
+        {
+            return;
+        }
+
+        if (c.gameObject.tag == "Player")
+        {
+            var target = c.gameObject.transform;
+            target.SetParent(this.transform);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D c)
+    {
+        if (!this.enabled)
+        {
+            return;
+        }
+
+        if (c.gameObject.tag == "Player")
+        {
+            var target = c.gameObject.transform;
+            var original = target.GetComponent<TransformState>().OriginalParent;
+            target.SetParent(original);
+          }
+    }
+
+    void Start()
+    {
+    }
+}
